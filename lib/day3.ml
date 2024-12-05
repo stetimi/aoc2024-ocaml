79cea49fd514385dfb2f4b8fe37ac8f051adf435
stetimi <123815784+stetimi@users.noreply.github.com>
2024-12-05 17:03:36 +0700

    Day 3, part B

--- lib/day3.ml
+++ lib/day3.ml
@@ -1,20 +1,35 @@
 open Core
 
-let sum_muls: string -> int = fun text ->
-  let re = Re2.create_exn {|mul\(([0-9]+),([0-9]+)\)|} in
-  let matches = Re2.get_matches re text in
-  let accumulate_matches total m = (
-    let first = Re2.Match.get_exn m ~sub:(`Index 1) |> Int.of_string_opt in
-    let second = Re2.Match.get_exn m ~sub:(`Index 2) |> Int.of_string_opt in
-    match (first, second) with
-    | (Some first, Some second) -> total + first * second
-    | _ -> total) in
-  match matches with
-  | Error _ -> 0
-  | Ok matches -> List.fold matches ~init:0 ~f:accumulate_matches
+let sum_muls re text =
+  let matches = Re.all re text |> List.map ~f:Re.Group.all in
+  let read_match (total, enabled) (matched_array: string array) = (
+    match (enabled, matched_array.(0)) with 
+    | (_, "do()") -> (total, true)
+    | (_,"don't()") -> (total, false)
+    | (false, _) -> (total, enabled)
+    | (true, _) -> 
+      let first = matched_array.(1) |> Int.of_string_opt in
+      let second = matched_array.(2) |> Int.of_string_opt in
+      match (first, second) with 
+      | (Some first, Some second) -> (total+first*second, enabled)
+      | _ -> (total, enabled)
+  ) in
+  List.fold_left matches ~init:(0, true) ~f:read_match |> fst
+
+let re_mul =
+  let re_digits = Re.(rep1 digit) in
+  Re.(seq [str "mul("; group re_digits; char ','; group re_digits; str ")"])
+
+let part_a_sum_muls = sum_muls (Re.compile re_mul)
+
+let part_b_sum_muls = 
+  let re_do = Re.str "do()" in
+  let re_dont = Re.str "don't()" in
+  let re_expr = Re.alt [re_mul; re_do; re_dont] in  
+  sum_muls (Re.compile re_expr)
 
 let part_a filename =
-  In_channel.read_all filename
-  |> sum_muls
+  In_channel.read_all filename |> part_a_sum_muls
 
-let part_b _filename = 0
+let part_b filename =
+  In_channel.read_all filename |> part_b_sum_muls
--- test/test_day3.ml
+++ test/test_day3.ml
@@ -4,7 +4,7 @@ open Aoc2024
 let assert_int_equals = assert_equal ~printer: Int.to_string 
 
 let day3_tests = "day 3 test suite" >::: [
-  "sum_muls on string containing no mul()" >:: (fun _ -> assert_int_equals 0 (Day3.sum_muls "no mul(45,int) here"));
-  "sum_muls on string containing one mul()" >:: (fun _ -> assert_int_equals 2520 (Day3.sum_muls "there is mul(45,56) here"));
-  "sum_muls on string containing two mul()" >:: (fun _ -> assert_int_equals 41 (Day3.sum_muls "there mul(5,4) is mul(7,3) here"));
+  "part a on string containing one mul()" >:: (fun _ -> assert_int_equals 2520 (Day3.part_a_sum_muls "there is mul(45,56) here"));
+  "part a on string containing two mul()" >:: (fun _ -> assert_int_equals 41 (Day3.part_a_sum_muls "there mul(5,4) is mul(7,3) here"));
+  "part b on example string" >:: (fun _ -> assert_int_equals 161 (Day3.part_b_sum_muls "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"));
 ]
