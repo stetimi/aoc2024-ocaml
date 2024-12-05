816cdb0f4d327d132248c55668f24da780d88729
stetimi <123815784+stetimi@users.noreply.github.com>
2024-12-05 11:26:07 +0700

    Day 3, install re2

--- lib/day3.ml
+++ lib/day3.ml
@@ -1,3 +1,20 @@
-let part_a _filename = 0
+open Core
+
+let sum_muls: string -> int = fun text ->
+  let re = Re2.create_exn {|mul\(([0-9]+),([0-9]+)\)|} in
+  let matches = Re2.get_matches re text in
+  let accumulate_matches total m = (
+    let first = Re2.Match.get_exn m ~sub:(`Index 1) |> Int.of_string_opt in
+    let second = Re2.Match.get_exn m ~sub:(`Index 2) |> Int.of_string_opt in
+    match (first, second) with
+    | (Some first, Some second) -> total + first * second
+    | _ -> total) in
+  match matches with
+  | Error _ -> 0
+  | Ok matches -> List.fold matches ~init:0 ~f:accumulate_matches
+
+let part_a filename =
+  In_channel.read_all filename
+  |> sum_muls
 
 let part_b _filename = 0
--- test/test_day3.ml
+++ test/test_day3.ml
@@ -1,6 +1,10 @@
 open OUnit2
 open Aoc2024
 
+let assert_int_equals = assert_equal ~printer: Int.to_string 
+
 let day3_tests = "day 3 test suite" >::: [
-  "dummy" >:: (fun _ -> assert_equal 0 (Day3.part_a ""));
+  "sum_muls on string containing no mul()" >:: (fun _ -> assert_int_equals 0 (Day3.sum_muls "no mul(45,int) here"));
+  "sum_muls on string containing one mul()" >:: (fun _ -> assert_int_equals 2520 (Day3.sum_muls "there is mul(45,56) here"));
+  "sum_muls on string containing two mul()" >:: (fun _ -> assert_int_equals 41 (Day3.sum_muls "there mul(5,4) is mul(7,3) here"));
 ]
