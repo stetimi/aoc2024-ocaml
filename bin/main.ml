41d427c36e9b65bd6e5aa764e6fd1acdf3ad9d02
stetimi <123815784+stetimi@users.noreply.github.com>
2024-12-09 14:53:30 +0800

    Commit accidentally ignored bin folder

---
+++ bin/main.ml
@@ -0,0 +1,14 @@
+open Core
+open Aoc2024
+
+let print_result (day: int) first second  =
+  let input = [%string "inputs/day%{day#Int}.txt"] in
+  print_endline [%string "Day %{day#Int}: part A: %{(first input)#Int}, part B: %{(second input)#Int}"]
+
+let () = 
+  print_result 1 Day1.part_a Day1.part_b;
+  print_result 2 Day2.part_a Day2.part_b;
+  print_result 3 Day3.part_a Day3.part_b;  
+  print_result 4 Day4.part_a Day4.part_b;
+  print_result 5 Day5.part_a Day5.part_b;
+  print_result 6 Day6.part_a Day6.part_b;
