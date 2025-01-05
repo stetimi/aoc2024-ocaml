9e92e197734d1fad9a64b30c4551807c191a216e
stetimi <123815784+stetimi@users.noreply.github.com>
2025-01-05 12:08:04 +0800

    Day 9

--- lib/day9.ml
+++ lib/day9.ml
@@ -1,119 +1,119 @@
 open Core
+open Tools
 
 let space = -1
 
-type file = {id: int; len: int} [@@deriving show]
+type disk = int array
 
-type sector =
-| Space of int
-| File of file [@@deriving show]
-
-type disk = sector array
-
-(* DEBUGGING *)
-let show_sector = function
-| Space len -> String.init len ~f:(Fn.const '.') 
-| File {id; len} -> String.init len ~f:(Fn.const (Char.of_int_exn (48+id)))
-
-let show_disk disk =
-  String.concat_array (Array.map disk ~f:show_sector) ~sep:"" 
-
-let dump_disk filename disk =
-  let sector_to_ints = function
-  | Space n -> Array.init n ~f:(Fn.const (-1))
-  | File {id; len} -> Array.init len ~f:(Fn.const id) in
-  let data = Array.concat_map disk ~f:sector_to_ints |> Array.map ~f:Int.to_string |> String.concat_array ~sep:"," in
-  Out_channel.write_all filename ~data
-(* END DEBUGGING *)
-
-let to_disk (disk_map: char array): disk =
-  Array.mapi disk_map
+let to_disk (disk_map: string) ~(on_space: int -> int -> unit): disk =
+  let disk_map = String.to_array disk_map in
+  let array_index = ref 0 in
+  Array.concat_mapi disk_map
     ~f:(fun i ch ->
       let len = Char.to_int ch - 48 in
-      if i % 2 = 0
-        then let id = i / 2 in File {id; len}
-        else Space len
+      let arr = if i % 2 = 0
+        then let id = i / 2 in Array.create ~len id
+        else (
+          on_space !array_index len;
+          Array.create ~len space
+        ) in
+        array_index := !array_index + len;
+      arr
     )
 
-let copy_back disk space_len end_i =
-  let rec go () = match disk.(!end_i) with
-  | Space _ -> (
-      end_i := !end_i - 1;
-      go ()
-  )
-  | File {id; len} -> (
-      match space_len with
-      | _ when space_len < len -> (
-            disk.(!end_i) <- File {id; len=len-space_len};
-            (id, space_len)
-          )
-      | _ -> (
-            end_i := (!end_i) - 1;
-            (id, len)
-          )
-  ) in
-  go ()
+let defragment_by_byte (disk: disk): int =
+  let read_ptr = ref (Array.length disk - 1) in
+  let write_ptr = ref 0 in
+  while !write_ptr < !read_ptr do
+    while disk.(!write_ptr) <> space do
+      incr write_ptr
+    done;
+    while disk.(!read_ptr) = space do
+      decr read_ptr
+    done;
+    disk.(!write_ptr) <- disk.(!read_ptr);
+    incr write_ptr;
+    decr read_ptr;
+  done;
+  !write_ptr
 
-let defragment_by_byte (disk: disk): disk =
-  let start_i = ref 0 in
-  let end_i = ref (Array.length disk - 1) in
-  let defragged = CCVector.create () in
-  let copy () = match disk.(!start_i) with
-  | File f -> (
-      start_i := !start_i + 1;
-      CCVector.push defragged (File f)
-  )
-  | Space space_len -> (
-      let (id, file_len) = copy_back disk space_len end_i in
-      if space_len > file_len
-        then disk.(!start_i) <- Space (space_len - file_len)
-        else start_i := !start_i + 1;
-      CCVector.push defragged (File {id; len=file_len})
-  ) in
-  let rec run () =
-    if !start_i < !end_i
+let pop_space_that_fits len index spaces_by_len =
+  let pop_first_index spaces len =
+    let spaces = Set.remove_index spaces 0 in
+    if Set.is_empty spaces 
+        then Hashtbl.remove spaces_by_len len
+        else
+          Hashtbl.set spaces_by_len ~key:len ~data:spaces in
+  let entry_with_min_id = Hashtbl.filter_keys spaces_by_len ~f:(fun l -> l >= len)
+  |> Hashtbl.to_alist 
+  |> List.min_elt ~compare:(fun (_, i1) (_, i2) -> on Set.min_elt_exn Int.compare i1 i2) in
+  match entry_with_min_id with
+  | None -> None
+  | Some (spaces_len, space_indices) ->
+    let min_space_index = Set.min_elt_exn space_indices in
+    if min_space_index < index 
       then (
-        copy ();
-        run ()
-      ) else (
-        CCVector.push defragged disk.(!start_i)
-      ) in  
-  run ();
-  CCVector.to_array defragged
+        pop_first_index space_indices spaces_len;
+        Some (spaces_len, min_space_index)
+      ) else None
 
-let maps (disk: disk) =
-  Array.foldi disk ~init:(Map.empty (module Int), []) ~f:(fun i (spaces_map, indexed_files) sector ->
-    match sector with
-    | Space len when len <> 0 -> Map.add_multi spaces_map ~key:len ~data:i, indexed_files
-    | Space _ -> spaces_map, indexed_files
-    | File f -> spaces_map, (i, f) :: indexed_files
-  )
-  |> Tuple2.map_fst ~f:(Map.map ~f:List.rev)
+let add_space len index spaces_by_len =
+  Hashtbl.update spaces_by_len len ~f:(function
+    | None -> Set.singleton (module Int) index
+    | Some spaces -> Set.add spaces index)
 
-let defragment_by_sector (disk: disk) =
-  let _spaces_map, _indexed_files = maps disk in
-  disk
+let defragment_by_sector (disk: disk) spaces_by_len: unit =
+  let lowest_block_copied = ref Int.max_value in
+  let read_ptr = ref (Array.length disk - 1) in
+  let read_block () =
+    while disk.(!read_ptr) = space || disk.(!read_ptr) >= !lowest_block_copied do
+      decr read_ptr;
+    done;
+    let first = disk.(!read_ptr) in
+    let len = ref 0 in
+    while !read_ptr >= 0 && disk.(!read_ptr) = first do
+      decr read_ptr;
+      incr len;
+    done;
+    lowest_block_copied := first;
+    (first, !len) in
+  while !read_ptr >= 0 do
+    let (block_id, block_len) = read_block () in
+    match pop_space_that_fits block_len (!read_ptr) spaces_by_len with 
+    | None -> ( 
+        ()
+      )
+    | Some (len, index) -> (
+        for i = 0 to block_len - 1 do
+          disk.(index + i) <- block_id;
+          disk.(!read_ptr + i + 1) <- space;
+        done;
+        if len > block_len then
+          add_space (len - block_len) (index + block_len) spaces_by_len;
+        ()
+      )
+  done
 
-let sum_range index len =
-  let sum n = n * (n - 1) / 2 in
-  sum (index + len) - sum index
-
-let checksum (disk: sector list): int =
-  let sector_checksum acc index = function
-  | Space len -> (acc, index + len)
-  | File {id; len} -> (acc + id * (sum_range index len), index + len) in
-  List.fold_left disk ~init:(0,0) ~f:(fun (acc, index) -> sector_checksum acc index) |> fst
+let checksum (disk: disk) (limit: int): int =
+  let sum = ref 0 in
+  for i = 0 to limit do
+    let id = disk.(i) in
+    if id <> space then sum := !sum + i * id;
+  done;
+  !sum
 
 let part_a filename = 
-  let disk = In_channel.read_all filename |> String.to_array |> to_disk in
-  let defragged = defragment_by_byte disk in
-  checksum (Array.to_list defragged)
-
-let disk () = In_channel.read_all "test/test_inputs/day9.txt" |> String.to_array |> to_disk
+  let disk = In_channel.read_all filename |> to_disk ~on_space:(fun _ _ -> ()) in
+  let endi = defragment_by_byte disk in
+  checksum disk endi
 
 let part_b filename = 
-  let disk = In_channel.read_all filename |> String.to_array |> to_disk in
-  let _defragged = defragment_by_sector disk in
-  2858
-  (* checksum (Array.to_list defragged) *)
-
+  let spaces_by_len = Hashtbl.create (module Int) in
+  let on_space index len = Hashtbl.change spaces_by_len len ~f:(fun ids ->
+    if len = 0 then None
+    else match ids with
+      | None -> Some (Set.singleton (module Int) index)
+      | Some ids -> Some (Set.add ids index)) in
+  let disk = In_channel.read_all filename |> to_disk ~on_space in
+  defragment_by_sector disk spaces_by_len;
+  checksum disk (Array.length disk - 1)
